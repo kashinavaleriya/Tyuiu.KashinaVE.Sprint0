294825b72b1268508b9443561159507d9ac82f1a
Kashinavaleriya <kashina11991@gmail.com>
2026-09-09 21:16:48 +0500

    Task1

---
+++ Tyuiu.KashinaVe.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+string name = "Лера";
+int age = 18;
+Console.WriteLine("Здравствуйте меня зовут " + name + " мне " + age + " лет ");
+Console.ReadKey();
