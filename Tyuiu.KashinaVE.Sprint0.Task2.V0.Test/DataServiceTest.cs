7515e9b4e3e466bfa490757d2345d1151f0c7b29
Kashinavaleriya <kashina11991@gmail.com>
2026-09-11 01:07:56 +0500

    Task2

---
+++ Tyuiu.KashinaVE.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.KashinaVE.Sprint0.Task2.V0.Lib;
+namespace Tyuiu.KashinaVE.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            var name = "Валерия";
+            var res = DataService.GetMessage(name);
+
+            Assert.AreEqual("Привет, Валерия", res);
+        }
+    }
+}
---
+++ Tyuiu.KashinaVE.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Tyuiu.KashinaVE.Sprint0.Task2.V0.Lib;
+Console.WriteLine(DataService.GetMessage("Валерия"));
+Console.ReadKey();
+Console.ReadKey();
