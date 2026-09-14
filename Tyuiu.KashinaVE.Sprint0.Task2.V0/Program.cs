58dac340b9b6e1a84a0643f5326d1207a9762138
Kashinavaleriya <kashina11991@gmail.com>
2026-09-14 19:36:01 +0500

    Task2

--- Tyuiu.KashinaVE.Sprint0.Task2.V0/Program.cs
+++ Tyuiu.KashinaVE.Sprint0.Task2.V0/Program.cs
@@ -1,5 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Tyuiu.KashinaVE.Sprint0.Task2.V0.Lib;
-Console.WriteLine(DataService.GetMessage("Валерия"));
-Console.ReadKey();
-Console.ReadKey();
+namespace Tyuiu.KashinaVE.Sprint0.Task2.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.GetMessage("Валерия"));
+            Console.ReadKey();
+        }
+    }
+}
