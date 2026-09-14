6953c78e20fa57b6a5d5150a46041f84250922d6
Kashinavaleriya <kashina11991@gmail.com>
2026-09-14 18:26:42 +0500

    Task1

--- Tyuiu.KashinaVe.Sprint0.Task1.V0/Program.cs
+++ Tyuiu.KashinaVe.Sprint0.Task1.V0/Program.cs
@@ -1,6 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-string name = "Лера";
-int age = 18;
-Console.WriteLine("Здравствуйте меня зовут " + name + " мне " + age + " лет ");
-Console.ReadKey();
+﻿namespace TYuiu.KashinaVE.Sprint0.Task0.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            string name = "Лера";
+            int age = 18;
+            Console.WriteLine("Здравствуйте меня зовут " + name + " мне " + age + " лет.");
+            Console.ReadKey();
+        }
+    }
+}
